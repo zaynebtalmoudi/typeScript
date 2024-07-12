5d58c5c22dc07bb443ba2b16a22ee8c8b444bdaa
Zayneb Telmoudi <zayneb@Imens-MacBook-Pro.local>
2024-07-12 23:18:44 +0100

    typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greeting from './greeting';
+import Counter from './counter';
+function App() {
+  return (
+    <div className="App">
+     <Greeting name='hamza' />
+     <Counter />
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/counter.tsx
@@ -0,0 +1,29 @@
+import { count } from 'console';
+import React, { Component } from 'react';
+
+
+
+interface counterState {
+  count: number;
+}
+
+class Counter extends Component< {}, counterState> {
+  state: counterState = {
+    count: 0,
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
